600776bc885311a5005f9c8d5bb0cac20f5f7c19
Karolina Mikulec <karolinaa.mikulec@gmail.com>
2025-05-08 22:46:11 +0200

    Your commit message here

---
+++ test_cleaning.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from processor.cleaning_utils import load_csv, clean_df
+
+df = load_csv("data/raw/medical_appointments.csv")
+print("Before:", df.shape)
+df_clean = clean_df(df)
+print("After: ", df_clean.shape)
+print(df_clean.head())
